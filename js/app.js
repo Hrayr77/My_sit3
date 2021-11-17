5b1ece76b50059f973a3f09376686486eb6c6774
Hrayr77 <79442635+Hrayr77@users.noreply.github.com>
2021-11-17 19:10:20 +0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,37 @@
+// const arr  = [
+//     {
+//         name:  'poxos1'
+//     },
+//     {
+//         name:  'poxos2'
+//     },
+//     {
+//         name:  'poxos3'
+//     },
+//     {
+//         name:  'poxos4'
+//     },
+//     {
+//         name:  'poxos5'
+//     },
+//     {
+//         name:  'poxos6'
+//     },
+// ];
+
+// const addToObject = array  => {
+//     for(let val = 0; val <  array.length; val++){
+//         Object.assign((array[val]),  {LastName: 'poxosyan'});
+//         array[val].LastName += val +1;
+//     }
+// console.log(arr);
+// }
+// addToObject(arr);
+
+// function howManySeconds(hours) {
+// 	let a = 60;
+//     let b = 60;
+//     let  x =  a * b; 
+//     console.log(x);
+// };
+// console.log(howManySeconds(1));
